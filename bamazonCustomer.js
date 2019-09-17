e3b3cecfd93fe59b8565bbeb7f9eea6b37f6e558
Jeremy Chiu <jeremybarotchiu@gmail.com>
2019-09-17 18:45:38 +1000

    asdf

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -5,7 +5,7 @@ var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "guitar87",
+    password: "",
     database: "bamazon_db"
 });
 
@@ -17,60 +17,60 @@ connection.connect(function (err) {
 function start() {
     connection.query("SELECT * FROM products", function (err, res) {
         console.table(res);
-                if (err) throw err;
-                inquirer.prompt([{
-                            name: "choice",
-                            type: "rawlist",
-                            choices: function () {
-                                var choiceArray = [];
-                                for (i = 0; i < res.length; i++) {
-                                    choiceArray.push(res[i].product_name);
-                                }
-                                return choiceArray;
-                            },
-                            message: "Enter the product ID you would like to purchase:"
-                        },
-                        {
-                            name: "quantity",
-                            type: "input",
-                            message: "How many units would you like to buy?",
-                            validate: function (value) {
-                                if (isNaN(value) === false) {
-                                    return true;
-                                }
-                                return false;
-                            }
-                        }
-                    ])
-                    .then(function (answer) {
-                        var chosenItem;
+        if (err) throw err;
+        inquirer.prompt([{
+                    name: "choice",
+                    type: "rawlist",
+                    choices: function () {
+                        var choiceArray = [];
                         for (i = 0; i < res.length; i++) {
-                            if (res[i].product_name === answer.choice) {
-                                chosenItem = res[i];
-                            }
+                            choiceArray.push(res[i].product_name);
                         }
-                        if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
-                            var deducted = chosenItem.stock_quantity - answer.quantity;
-                            connection.query(
-                                "UPDATE products SET ? WHERE ?",
-                                [{
-                                        stock_quantity: deducted
-                                    },
-                                    {
-                                        item_id: chosenItem.item_id
-                                    }
-                                ],
-                                function (err) {
-                                    if (err) throw err;
-                                    var total = answer.quantity * chosenItem.price;
-                                    console.log("Order successful! Total cost is: $" + total);
-                                    start();
-                                }
-                            );
-                        } else {
-                            console.log("Insufficient stock!");
+                        return choiceArray;
+                    },
+                    message: "Enter the product ID you would like to purchase:"
+                },
+                {
+                    name: "quantity",
+                    type: "input",
+                    message: "How many units would you like to buy?",
+                    validate: function (value) {
+                        if (isNaN(value) === false) {
+                            return true;
+                        }
+                        return false;
+                    }
+                }
+            ])
+            .then(function (answer) {
+                var chosenItem;
+                for (i = 0; i < res.length; i++) {
+                    if (res[i].product_name === answer.choice) {
+                        chosenItem = res[i];
+                    }
+                }
+                if (chosenItem.stock_quantity > parseInt(answer.quantity)) {
+                    var deducted = chosenItem.stock_quantity - answer.quantity;
+                    connection.query(
+                        "UPDATE products SET ? WHERE ?",
+                        [{
+                                stock_quantity: deducted
+                            },
+                            {
+                                item_id: chosenItem.item_id
+                            }
+                        ],
+                        function (err) {
+                            if (err) throw err;
+                            var total = answer.quantity * chosenItem.price;
+                            console.log("Order successful! Total cost is: $" + total);
                             start();
                         }
-                    });
-        });
+                    );
+                } else {
+                    console.log("Insufficient stock!");
+                    start();
+                }
+            });
+    });
 }
